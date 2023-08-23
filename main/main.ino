d9b565ff5542c9ef34a4202e33761870b8e0a2d7
Saugat Neupane <saugatn3@gmail.com>
2023-08-23 11:26:47 +0545

    integrated call feature and blynk connection in a single file

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,4 @@
-#include <SoftwareSerial.h>
-
+int called=0;
 #define SIM800L_AXP192_VERSION_20200327
 #define BLYNK_TEMPLATE_ID "TMPL6HSATnYDn"
 #define BLYNK_TEMPLATE_NAME "gps"
@@ -10,63 +9,78 @@
 #include <TinyGPS++.h>
 #include <WiFi.h>
 #include <BlynkSimpleEsp32.h>
+#include <SoftwareSerial.h> // Include the SoftwareSerial library
 
-SoftwareSerial gsmSerial(MODEM_RX, MODEM_TX);
-
-#define TINY_GSM_RX_BUFFER 1024
-#define WIFI_SSID "King's LANding"
-#define WIFI_PASSWORD "lumbini@534"
+//const char apn[]      = "web"; 
+//const char gprsUser[] = "";
+//const char gprsPass[] = ""; 
+#define WIFI_SSID "Seminar@lbef"
+#define WIFI_PASSWORD "lbef##apu@@"
 #define MAX_TIME_FOR_INTERNET_RESET 10
-#define SerialAT Serial2
+#define SerialAT Serial1 // Use the same hardware serial port for both GSM and GPS
+
+SoftwareSerial SerialGPS(35, 34); // RX, TX pins for the SoftwareSerial (GPS)
+
 int watchdogWiFiConnectCounter = 0;
 
-#define CALL_TARGET "+9779864063381"
 // Create a TinyGPS++ object to parse GPS data
 TinyGPSPlus gps;
 
-//TinyGsm modem(Serial1);
 TinyGsm modem(SerialAT);
 
-void call(){
-    gsmSerial.println("Initializing modem...");
-    modem.init();
+// Set phone numbers, if you want to test SMS and Calls
+#define CALL_TARGET "MOBILE_NUMBER"
 
-    // Swap the audio channels
-    gsmSerial.print("AT+CHFA=1\r\n");
-    delay(2);
-    //Set ringer sound level
-    gsmSerial.print("AT+CRSL=100\r\n");
-    delay(2);
+void call() {
+  Serial.println("Initializing modem...");
+  modem.init();
 
-    //Set loud speaker volume level
-    gsmSerial.print("AT+CLVL=100\r\n");
-    delay(2);
+  // Calling line identification presentation
+  SerialAT.print("AT+CLIP=1\r\n");
+  delay(10000);
+   modem.init();
+  Serial.print("Calling:");
+  Serial.println(CALL_TARGET);
 
-    // Calling line identification presentation
-    gsmSerial.print("AT+CLIP=1\r\n");
-    delay(10000);
+  bool res = modem.callNumber(CALL_TARGET);
 
-    gsmSerial.print("Calling:");
-    gsmSerial.println(CALL_TARGET);
+  Serial.print("Call:");
+  Serial.println(res? "ok" :"fail");
 
-    bool res = modem.callNumber(CALL_TARGET);
+  // Hang up after 20 seconds
+  delay(20000);
 
-    gsmSerial.print("Call:");
-    gsmSerial.println(res ? "OK" : "fail");
-
-    // Hang up after 20 seconds
-    delay(20000);
-    res=modem.callHangup();
-    gsmSerial.print("Call:");
-    gsmSerial.println(res ? "OK" : "fail");
-    ESP.restart();
+  res = modem.callHangup();
+  Serial.println(res ? "Hang up: OK" : "Hang up: fail");
+  called=1;
 }
-
 
 void setup() {
   Serial.begin(115200); // Initialize Serial Monitor for debugging
-  SerialAT.begin(9600, SERIAL_8N1, 27, 26); // Initialize Serial2 for GPS communication
-  gsmSerial.begin(9600); 
+  SerialGPS.begin(9600); // Initialize SoftwareSerial for GPS
+
+  //if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
+    //Serial.println(" fail");
+  //} else {
+   // Serial.println("Pass");
+    //}
+    
+  // Connect to Wi-Fi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+
+    Serial.println("Connecting to WiFi...");
+    // if SMS received, SMS = RESET, restart
+    watchdogWiFiConnectCounter++;
+
+    if (watchdogWiFiConnectCounter == MAX_TIME_FOR_INTERNET_RESET) {
+      // TODO: Send SMS that GPRS connection failed.
+      ESP.restart();
+    }
+  }
+  Serial.println("Connected to WiFi!");
 
   if (setupPMU() == false) {
         Serial.println("Setting power error");
@@ -75,41 +89,28 @@ void setup() {
     // Some start operations
     setupModem();
 
-  // Connect to Wi-Fi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-
-    Serial.println("Connecting to WiFi...");
-    // if sms received , sms = RESET , restart
-    watchdogWiFiConnectCounter++;
-
-    if (watchdogWiFiConnectCounter == MAX_TIME_FOR_INTERNET_RESET) {
-      // TODO : Send sms that gprs connection failed.
-      ESP.restart();
-     }
-
-  }
-  Serial.println("Connected to WiFi!");
-
+    // Set GSM module baud rate and UART pins
+    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  //Blynk.begin(auth, modem, apn, user, pass);
   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop() {
+  if(!called){
+   call();
+    Serial.print("just called");
+  }
+  
   // Keep reading data from the GPS module
-  call();
-  while (Serial2.available() > 0) {
-    gps.encode(Serial2.read());
+  while (SerialGPS.available() > 0) {
+    gps.encode(SerialGPS.read());
   }
 
-  // Check if we have valid GPS data
   if (gps.location.isValid()) {
-
     // gps.location.isUpdated()
     // Get latitude and longitude in degrees
 
-    double latitude = round(gps.location.lat()* 10000.0) / 10000.0;
+    double latitude = round(gps.location.lat() * 10000.0) / 10000.0;
     double longitude = round(gps.location.lng() * 10000.0) / 10000.0;
 
     // Get altitude in meters above sea level
@@ -124,16 +125,15 @@ void loop() {
     Serial.print(", Altitude: ");
     Serial.print(altitude);
     Serial.print(" meters");
-      Serial.print(", Speed: ");
+    Serial.print(", Speed: ");
     Serial.print(speed);
     Serial.println(" kmph");
     // Send latitude and longitude to Blynk virtual pins V1 and V2
     Blynk.virtualWrite(V3, altitude);
     Blynk.virtualWrite(V4, speed);
-    Blynk.virtualWrite(V2,longitude,latitude);
-        Blynk.virtualWrite(V1,speed);
-
+    Blynk.virtualWrite(V2, longitude, latitude);
+    Blynk.virtualWrite(V1, speed);
   }
-
-  Blynk.run();
+  //Blynk.begin(auth, modem, apn, user, pass);
+  Blynk.run();  
 }
